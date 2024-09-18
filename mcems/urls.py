5bfd8f6eb6034d0f22e2f2233b69ee4e13512cf3
Maneshnarayanan <maneshk27@gmail.com>
2024-09-18 12:03:00 +0530

    admin interface changed

--- mcems/urls.py
+++ mcems/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('companies/', include('companies.urls')),
     path('', include('users.urls')),
